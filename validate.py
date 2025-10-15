15d49373f8398d58d01d2482a6e89784a34599ae
Escobar Viegas, Maximiliano Fernando <maxescovi@gmail.com>
2025-10-15 19:00:34 +0200

    chore: derive occupancy

--- validate.py
+++ validate.py
@@ -11,40 +11,47 @@ from entise.constants.columns import Columns
 from entise.core.generator import TimeSeriesGenerator
 from entise.methods.auxiliary.internal.strategies import InternalOccupancy
 
+
 cwd="."
 objects = pd.read_csv(os.path.join(cwd, "objects_validation.csv"))
+demand_folder="data/validation/demand"
 data = {}
-#common_data_folder = "common_data"
-#for file in os.listdir(os.path.join(cwd, common_data_folder)):
-#    if file.endswith(".csv"):
-#        name = file.split(".")[0]
-#        data[name] = pd.read_csv(os.path.join(os.path.join(cwd, common_data_folder, file)), parse_dates=True)
 
-data_folder = "data"
-for file in os.listdir(os.path.join(cwd, data_folder)):
-    if file.endswith(".csv"):
-        name = file.split(".")[0]
-        data[name] = pd.read_csv(os.path.join(os.path.join(cwd, data_folder, file)), parse_dates=True)
-print("Loaded data keys:", list(data.keys()))
+#DERIVE OCCUPANCY SCHEDULE
 
-# Instantiate and configure the generator
-gen = TimeSeriesGenerator(logging_level=logging.WARNING)
+processed_ids = set()
+for idx,obj in objects.iterrows():
+    obj_id = str(obj["id"])
 
-# Define the building ID to process and visualize
+    if obj_id in processed_ids:
+        continue
 
-# Filter objects to only include one object (for debugging)
-#print(f"Processing only object with ID: {building_id}")
+    matching_files = [f for f in os.listdir(os.path.join(cwd, demand_folder)) if f.startswith(obj_id)]
 
-gen.add_objects(objects)
+    if not matching_files:
+        continue
+    
+    file = matching_files[0]  # only one match
+    df_demand = pd.read_csv(os.path.join(cwd, demand_folder, file), parse_dates=True)
+    
+    data[Columns.DEMAND]=df_demand
 
-# Generate time series
-summary, df = gen.generate(data, workers=1)
+    print("Loaded data keys:", list(data.keys()))
 
+    # Instantiate and configure the generator
+    gen = TimeSeriesGenerator(logging_level=logging.WARNING)
 
+    gen.add_objects(objects)
+
+    # Generate Occupancy time series
+    summary, df = gen.generate(data, workers=1)
+
+    # Print summary
+    print("Summary occupancy:")
+    print(summary)
+
+    df[int(obj_id)][Types.OCCUPANCY].to_csv(f"data/validation/occupancy/{Columns.OCCUPANCY_GEOMA}/{obj_id}.csv")
+
+    processed_ids.add(obj_id)
 
 #df_internal_gains=InternalOccupancy().generate(obj=objects_filtered,data=df[building_id])
-
-
-# Print summary
-print("Summary occupancy:")
-print(summary)
